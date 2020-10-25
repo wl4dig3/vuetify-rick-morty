5d25f357826995d45308b56c831cd8e60f5964f6
Wladimir Guevara <wladige@gmail.com>
2020-10-25 17:55:08 -0300

    creando proyecto en nuxt-vuetify rick and morty

---
+++ pages/barra.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-container grid-list-xs>
+    <V-row>
+      <v-col>
+        <v-card class="mx-auto" max-width="344">
+        <v-img
+          src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg"
+          height="200px"
+        ></v-img>
+
+        <v-card-title> Top western road trips </v-card-title>
+
+        <v-card-subtitle> 1,000 miles of wonder </v-card-subtitle>
+      </v-card>
+      </v-col>
+    </V-row>
+  </v-container>
+</template>
